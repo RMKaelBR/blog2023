2b576b8d9d87de5bf7e035f8980bd6e6f0cb5eda
RMKaelBR <rmbresurreccion@gmail.com>
2023-07-04 20:26:43 +0800

    + Finished Update, and Delete features for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,13 +12,31 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(title: "...", body: "...")
+    @post = Post.new(post_params)
 
     if @post.save
       redirect_to @post
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path, status: :see_other
   end
 
   private
